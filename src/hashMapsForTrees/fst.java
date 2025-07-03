340bbcd92360248c9170e2361644ba442f8cb5cd
Shivansu77 <shivansubisht77@gmail.com>
2025-07-03 12:32:01 +0530

    hashmaps

--- src/hashMapsForTrees/fst.java
+++ src/hashMapsForTrees/fst.java
@@ -2,8 +2,20 @@ package hashMapsForTrees;
 import java.util.*;
 public class fst {
     public static void main(String[] args) {
-        String name="kunal";
-        int code=name.hashCode();
-        System.out.println("code: "+code);
+       HashMap<String,Integer> map=new HashMap<>();
+       map.put("kunal",89);
+        map.put("karan",99);
+        map.put("Rahul",94);
+        System.out.println(map);
+        //System.out.println(map.get("karan "));
+        //System.out.println(map.getOrDefault("Apoorva",71));
+        System.out.println(map.containsKey("karan"));
+        HashSet<Integer> set=new HashSet<>();
+        set.add(89);
+        set.add(94);
+        set.add(93);
+        set.add(89);
+        set.add(92);
+        System.out.println(set);
     }
 }
