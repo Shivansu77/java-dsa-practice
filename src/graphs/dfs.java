70b4a42928cc9481229875f8f684ce577f433555
Shivansu77 <shivansubisht77@gmail.com>
2025-07-09 16:33:20 +0530

    dfs for graph

--- src/graphs/dfs.java
+++ src/graphs/dfs.java
@@ -1,7 +1,27 @@
 package graphs;
 
 public class dfs {
-    public static void main(String[] args) {
-        System.out.println();
-    }
+
+        public ArrayList<Integer> dfs(ArrayList<ArrayList<Integer>> adj) {
+            int n=adj.size();
+            ArrayList<Integer>result=new ArrayList<>();
+            boolean []vis=new boolean[n];
+
+            for(int i=0;i<n;i++){
+                if(!vis[i]){
+                    helper(i,result,adj,vis);
+                }
+            }
+            return result;
+        }
+        public void helper(int i,ArrayList<Integer>result,ArrayList<ArrayList<Integer>> adj,boolean []vis){
+            vis[i]=true;
+            result.add(i);
+            for(Integer v:adj.get(i)){
+                if(!vis[v]){
+                    helper(v,result,adj,vis);
+                }
+            }
+        }
+
 }
