6ec2b602d634c84163ad409f43b48c9748ed9fc2
Shivansu77 <shivansubisht77@gmail.com>
2025-07-25 21:51:40 +0530

    connected pc

---
+++ src/graphs/makeConnected.java
@@ -0,0 +1,34 @@
+package graphs;
+
+public class makeConnected {
+    public int makeConnected1(int n, int[][] connections) {
+        if(connections.length<n-1)return -1;
+        List<List<Integer>> graph = new ArrayList<>();
+        boolean[]vis=new boolean[n];
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] edge : connections) {
+            int u = edge[0];
+            int v = edge[1];
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        int component=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                dfs(i,vis,graph);
+                component++;
+            }
+        }
+        return component-1;
+    }
+    public void dfs(int node,boolean[]vis,List<List<Integer>> graph ){
+        vis[node]=true;
+        for(int nbr:graph.get(node)){
+            if(!vis[nbr]){
+                dfs(nbr,vis,graph);
+            }
+        }
+    }
+}
