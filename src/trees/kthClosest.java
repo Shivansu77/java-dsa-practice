357c4174815cfc370c213ad37d46e1d33348eede
shivansu77 <shivansubisht77@gmail.com>
2025-08-05 22:58:00 +0530

    kth closest

---
+++ src/trees/kthClosest.java
@@ -0,0 +1,31 @@
+package trees;
+
+public class kthClosest {
+    public int[][] kClosest(int[][] points, int k) {
+        // Max heap: largest distance at top
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>(
+                (a, b) -> Integer.compare(distance(b), distance(a))
+        );
+
+        for (int[] point : points) {
+            maxHeap.offer(point);
+            if (maxHeap.size() > k) {
+                maxHeap.poll(); // remove farthest
+            }
+        }
+
+        // Prepare result array
+        int[][] result = new int[k][2];
+        int i = 0;
+        for (int[] point : maxHeap) {
+            result[i++] = point;
+        }
+
+        return result;
+    }
+
+    // Helper method to compute squared distance
+    private int distance(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+}
