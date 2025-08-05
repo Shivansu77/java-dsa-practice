caa20e1f160fa3b2c9ecfb22fea041cfe2ce8553
shivansu77 <shivansubisht77@gmail.com>
2025-08-05 22:51:50 +0530

    inorder

---
+++ src/trees/inorder.java
@@ -0,0 +1,17 @@
+package trees;
+
+public class inorder {
+    ArrayList<Integer> inOrder(Node root) {
+        // Code
+        ArrayList<Integer>list=new ArrayList<>();
+        helper(root,list);
+        return list;
+
+    }
+    public void helper(Node root, ArrayList<Integer>list){
+        if(root==null)return;
+        helper(root.left,list);
+        list.add(root.data);
+        helper(root.right,list);
+    }
+}
