037a191e7eb55fe5ac15fa82c6d9f6b9b745902d
shivansu77 <shivansubisht77@gmail.com>
2025-08-07 12:05:37 +0530

    dsu

---
+++ src/graphs/sdu.java
@@ -0,0 +1,40 @@
+package graphs;
+
+public class sdu {
+    private int[] parent;
+    private int[] rank;
+    private int n;
+
+    public DisjointUnionSets(int n) {
+        this.n = n;
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;   // each element is its own parent
+            rank[i] = 0;
+        }
+    }
+
+    public int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]); // path compression
+        }
+        return parent[x];
+    }
+
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+
+        // union by rank heuristic
+        if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else if (rank[rootY] < rank[rootX]) {
+            parent[rootY] = rootX;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+    }
+}
