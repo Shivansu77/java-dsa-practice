f7f4e8ae167f54c85afc4c2e0475b4f397cc3380
shivansu77 <shivansubisht77@gmail.com>
2025-08-09 21:48:41 +0530

    valid parentheses

---
+++ src/GFG/da99y.java
@@ -0,0 +1,23 @@
+package GFG;
+
+public class da99y {
+    static boolean isBalanced(String s) {
+        // code here
+        Stack<Character>stack=new Stack<>();
+        for(char ch:s.toCharArray() ){
+            if(ch=='[' || ch=='{' || ch=='('){
+                stack.push(ch);
+            }
+            else if(ch==']'){
+                if(stack.isEmpty() || stack.pop()!='[')return false;
+            }
+            if(ch=='}'){
+                if(stack.isEmpty() || stack.pop()!='{')return false;
+            }
+            if(ch==')'){
+                if(stack.isEmpty() || stack.pop()!='(')return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
