25712968aa22aee34cd7ba0114d3c4fd04d37368
shivansu77 <shivansubisht77@gmail.com>
2025-08-31 21:42:50 +0530

    Max Sum without Adjacents

---
+++ src/DP/maxsumwithoAdj.java
@@ -0,0 +1,17 @@
+package DP;
+
+public class maxsumwithoAdj {
+    int findMaxSum(int arr[]) {
+        // code here
+        int inc=arr[0];
+        int exc=0;
+
+        for(int i=1;i<arr.length;i++){
+            int ninc=exc+arr[i];
+            int nexc=Math.max(inc,exc);
+            inc=ninc;
+            exc=nexc;
+        }
+        return Math.max(inc,exc);
+    }
+}
